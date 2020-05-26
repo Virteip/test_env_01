f7bd7b2e341e074e95fa179d4028b1d47b882d67
Virteip <sergiopietri@gmail.com>
2020-05-26 13:57:47 -0500

    [Add]

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/testing-pipe', function (req, res) {
+		  res.send('Hello World!');
+		  });
+
+app.listen(process.env.port || 3000, function () {
+		  console.log('Example app listening on port 3000!');
+		  });
